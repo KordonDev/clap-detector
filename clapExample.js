0f218666159299b93639c5c94bdb1e725677ba57
Arne Maier <Arne_Maier@gmx.de>
2018-06-10 12:37:45 +0200

    Update example

--- clapExample.js
+++ clapExample.js
@@ -1,5 +1,5 @@
 // Require the module
-const clapDetector = require('./index.js');
+const audioDetector = require('./index.js');
 
 // Define configuration
 var clapConfig = {
@@ -11,16 +11,16 @@ var clapConfig = {
 };
 
 // Start clap detection
-clapDetector.start(clapConfig);
+audioDetector.start(clapConfig);
 
 // Register on clap event
-clapDetector.onDetection(function(history) {
+audioDetector.onDetection(function(history) {
     console.log('your callback code here ');
     console.dir(history);
 });
 
 // Register to a series of 3 claps occuring within 2 seconds
-clapDetector.onDetections(3, 2000, function(delay) {
+audioDetector.onDetections(3, 2000, function(delay) {
     console.log('your callback code here for multiple claps');
 });
 
